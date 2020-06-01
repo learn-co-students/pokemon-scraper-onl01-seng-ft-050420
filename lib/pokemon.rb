305ce5fc6d5ac1d888f87f8fce884a327f6853c1
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-01 17:00:48 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id: nil, name:, type:, db:)  
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    sql = "INSERT INTO pokemon (name, type, db) VALUES (?, ?, ?);"
+    @db[:conn].execute(sql, name, type, db)
+    @id = @db[:conn].execute("SELECT last_insert_rowid()")[0][0]
+  end
+    
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?;"
+    result = @db[:conn].execute(sql, id)
+    self.new(result[0], result[1], result[2], db)
+  end
+
 end
